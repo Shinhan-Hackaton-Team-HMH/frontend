2e260a2b423fc44d0cee54663bd943faef19e8c0
robin <nowrobin3@gmail.com>
2025-08-12 17:50:35 +0900

    fix: page callback with no run

--- src/components/kakaoRedirect/index.tsx
+++ src/components/kakaoRedirect/index.tsx
@@ -9,41 +9,39 @@ export default function KakaoRedirectClient() {
   const code = searchParams.get('code') ?? undefined;
   console.log(code);
   useEffect(() => {
-    // const run = async () => {
-    //   if (!code) {
-    //     console.error('인가 코드가 없습니다.');
-    //     router.replace('/?error=missing_code');
-    //     return;
-    //   }
-    //   console.log('리다이렉트 접속 (client)');
-    //   const backendUrl = `/proxy/auth/callback/kakao/token-exchange/code=${code}`;
-    //   try {
-    //     const postResponse = await fetch(backendUrl, {
-    //       method: 'POST',
-    //       headers: { 'Content-Type': 'application/json' },
-    //       body: JSON.stringify({ code }),
-    //       credentials: 'include', // 필요 시 쿠키 전달
-    //     });
-    //     if (!postResponse.ok) {
-    //       console.error('백엔드에서 토큰 교환 실패');
-    //       router.replace('/error=token_exchange_failed');
-    //       return;
-    //     }
-    //     const tokenData = await postResponse.json();
-    //     const token = tokenData.token as string | undefined;
-    //     if (!token) {
-    //       console.error('토큰이 응답에 없습니다.');
-    //       router.replace('/error=missing_token');
-    //       return;
-    //     }
-    //     router.replace('/');
-    //   } catch (error) {
-    //     console.error('네트워크 또는 기타 에러 발생:', error);
-    //     router.replace('/error=network_error');
-    //   }
-    // };
-    // // 즉시 실행
-    // run();
+    const run = async () => {
+      if (!code) {
+        console.error('인가 코드가 없습니다.');
+        router.replace('/?error=missing_code');
+        return;
+      }
+      console.log('리다이렉트 접속 (client)');
+      const backendUrl = `/proxy/auth/callback/kakao?code=${code}`;
+      try {
+        const postResponse = await fetch(backendUrl, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        });
+        if (!postResponse.ok) {
+          console.error('백엔드에서 토큰 교환 실패');
+          router.replace('/error=token_exchange_failed');
+          return;
+        }
+        const tokenData = await postResponse.json();
+        const token = tokenData.token as string | undefined;
+        if (!token) {
+          console.error('토큰이 응답에 없습니다.');
+          router.replace('/error=missing_token');
+          return;
+        }
+        router.replace('/');
+      } catch (error) {
+        console.error('네트워크 또는 기타 에러 발생:', error);
+        router.replace('/error=network_error');
+      }
+    };
+    // 즉시 실행
+    run();
   }, [code, router]);
 
   // 간단한 로딩 UI
